0993ea4f6adb6aa1cdae86ff3dbbd0ff6ef88aa7
Jeffrey Charles <jeffreycharles@gmail.com>
2017-05-06 22:15:49 -0400

    Fix casing

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import './css/App.css';
+import './css/app.css';
 
 import App from './components/App';
 
